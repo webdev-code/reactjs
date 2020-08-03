42f76f868359b0fed0841ff51005102b6c9f3c85
usa3a <usa3amustafa@gmail.com>
2020-08-03 16:56:33 +0500

    functional compoenets

--- src/index.js
+++ src/index.js
@@ -1,12 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import './App.css';
 
-ReactDOM.render(
-  <ul>
-    <li>Name : Usama</li>
-    <li>Course : React</li>
-    <li>Level : Beginner</li>
-  </ul>,
-  document.getElementById('root')
-);
+//functional Component
+function MyInfo() {
+  return (
+    <div className='container'>
+      <h1 className='name'>Usama Mustafa</h1>
+      <p className='bio'>I am Learing React From Scratch</p>
+      <ul className='cities'>
+        <li>Islamabad</li>
+        <li>Lahore</li>
+        <li>Rawalpindi</li>
+      </ul>
+    </div>
+  );
+}
+
+//rendering a functional Compenent in the render method
+ReactDOM.render(<MyInfo />, document.getElementById('root'));
+
+// redering a component in render method
+// ReactDOM.render(
+//   <ul>
+//     <li>Name : Usama</li>
+//     <li>Course : React</li>
+//     <li>Level : Beginner</li>
+//   </ul>,
+//   document.getElementById('root')
+// );
