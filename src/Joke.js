a9fd4b0cb59e164bfb70cde53a59b8ad7fd3f1f7
usa3a <usa3amustafa@gmail.com>
2020-08-05 21:06:34 +0500

    display property

--- src/Joke.js
+++ src/Joke.js
@@ -4,8 +4,18 @@ function Joke(props) {
   //   console.log(props.joke);
   return (
     <div>
-      <h1 className='question'>{props.joke.question}</h1>
-      <h2 className='punchLine'>{props.joke.punchLine}</h2>
+      <h1
+        style={{ display: props.joke.question ? 'block' : 'none' }}
+        className='question'
+      >
+        Question : {props.joke.question}
+      </h1>
+      <h2
+        style={{ color: !props.joke.question && '#e88888' }}
+        className='punchLine'
+      >
+        PunchLine : {props.joke.punchLine}
+      </h2>
       <hr />
     </div>
   );
