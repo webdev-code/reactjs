acd3cc30746d3fcf42310c1d8dc910877209a52a
usa3a <usa3amustafa@gmail.com>
2020-08-13 14:23:18 +0500

    update

--- src/App.js
+++ src/App.js
@@ -1,22 +1,35 @@
 import React from 'react';
-// import InlineStyling from './InlineStyling';
-// import MyInfo from './MyInfo';
-// import Header from './Header';
-// import MainContent from './MainContent';
-// import Footer from './Footer';
-// import ToDo from './ToDoPhase1';
+import InlineStyling from './InlineStyling';
+import MyInfo from './MyInfo';
+import Header from './Header';
+import MainContent from './MainContent';
+import Footer from './Footer';
+import ToDo from './ToDoPhase1';
 import JokeMain from './JokeMain';
+import Square from './squaremap';
+
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]; //array of numbers
+
+//map to create a new array 'square' which holds as many 'square' components as numbers array
+// square component is called against each 'num' and then stored in square array
+const square = numbers.map((num) => {
+  return <Square square={num * num} number={num} />;
+  // return <h1>{num * num}</h1>;
+  // return num * num;
+});
 
 function App() {
   return (
     <div>
-      {/* <MyInfo />
+      <MyInfo />
       <Header />
       <MainContent />
-      <Footer /> 
+      <Footer />
       <ToDo />
-      <InlineStyling />*/}
+      <InlineStyling />
       <JokeMain />
+      <h1>{square}</h1>
+      {square}
     </div>
   );
 }
---
+++ src/squaremap.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Square(props) {
+  return (
+    <div>
+      <h1>
+        the suqare of {props.number} is : {props.square}
+      </h1>
+    </div>
+  );
+}
+
+export default Square;
